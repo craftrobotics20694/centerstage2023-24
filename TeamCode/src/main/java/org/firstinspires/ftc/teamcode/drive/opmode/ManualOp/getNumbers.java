62c8ab2eca946b37f67f3cc0e1fa2b19fdeceba7
BruhGrums <123519055+craftrobotics20694@users.noreply.github.com>
2024-02-02 01:55:03 -0500

    Tuning and Manuel OP
    Tuning and Manuel OP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ManualOp/getNumbers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ManualOp/getNumbers.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.ManualOp;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Config
+@Autonomous(name = "Get nums", group = "comp")
 public class getNumbers extends OpMode {
 
 
@@ -46,14 +49,16 @@ public class getNumbers extends OpMode {
         if (gamepad2.circle) {
             gripServoB.setPosition(1);
             gripServoF.setPosition(1);
+            telemetry.addData("Servo PositionBWhile", gripServoB.getPosition());
+            telemetry.addData("Servo PositionBWhile", gripServoB.getPosition()*300);
 
         } else {
             gripServoB.setPosition(0);
             gripServoB.setPosition(0);
 
         }
-        telemetry.addData("Servo Position", gripServoB.getPosition());
-        telemetry.addData("Servo Position", gripServoB.getPosition()*300);
+        telemetry.addData("Servo PositionB", gripServoB.getPosition());
+        telemetry.addData("Servo PositionB", gripServoB.getPosition()*300);
 
 
         telemetry.addData("leftslide endcoderPOS", slideLeft.getCurrentPosition());
