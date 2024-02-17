8dd868bfec463542c77b0045c71c4a2bcc7072dc
BruhGrums <123519055+craftrobotics20694@users.noreply.github.com>
2024-02-16 23:35:49 -0500

    hope and pray lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -1,5 +1,17 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.config.Config;
@@ -21,9 +33,11 @@ import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
@@ -36,18 +50,6 @@ import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ACCEL;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_ACCEL;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_ANG_VEL;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MAX_VEL;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MOTOR_VELO_PID;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.RUN_USING_ENCODER;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.encoderTicksToInches;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kA;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
-import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
 
 /*
  * Simple mecanum drive hardware implementation for REV hardware.
@@ -72,6 +74,9 @@ public class SampleMecanumDrive extends MecanumDrive {
 
     private DcMotorEx leftFront, leftRear, rightRear, rightFront;
     private List<DcMotorEx> motors;
+
+    public Servo gripServoB, gripServoF,launchServo;
+    public DcMotorEx slideLeft, slideRight, wristMotor;
 
     private IMU imu;
     private VoltageSensor batteryVoltageSensor;
@@ -105,6 +110,19 @@ public class SampleMecanumDrive extends MecanumDrive {
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
+
+        gripServoF =hardwareMap.servo.get("gripServoF");
+        gripServoB =hardwareMap.servo.get("gripServoB");
+        launchServo = hardwareMap.servo.get("launchServo");
+
+        slideLeft = hardwareMap.get(DcMotorEx.class, "slideLeft");
+        slideRight = hardwareMap.get(DcMotorEx.class, "slideRight");
+        wristMotor = hardwareMap.get(DcMotorEx.class, "wristMotor");
+
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
 
         for (DcMotorEx motor : motors) {
             MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
@@ -308,4 +326,103 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static TrajectoryAccelerationConstraint getAccelerationConstraint(double maxAccel) {
         return new ProfileAccelerationConstraint(maxAccel);
     }
+
+    // Custom code from this point on
+
+    // Iterate over a list of motors and set them to a give mode
+    private void setMotorMode(DcMotorEx.RunMode mode, DcMotorEx... motors) {
+        // Iterate over each DcMotor object and set their motor mode
+        for (DcMotorEx motor : motors) {
+            motor.setMode(mode);
+        }
+    }
+
+    // Call setMotorMode() to turn off and reset the encoders on all slide motors
+    public void stopAndResetMotors() {
+        setMotorMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER, slideLeft, slideRight, wristMotor);
+    }
+
+    // Call setMotorMode() to turn on all slide motors
+    public void restartMotors() {
+        setMotorMode(DcMotorEx.RunMode.RUN_TO_POSITION, slideLeft, slideRight, wristMotor);
+    }
+
+    // Bundles all the functions needed to initialize the arm controls
+
+    public void initArm() {
+        stopAndResetMotors();
+        setBothGrip(false);
+        setSlideVelocity(2000, slideLeft, slideRight, wristMotor);
+        setHeight(0);
+        setExtension(0);
+        restartMotors();
+    }
+
+    // Set the target encoder position of the vertical slides
+    public void setHeight(int height) {
+        slideLeft.setTargetPosition(height);
+        slideRight.setTargetPosition(-height);
+    }
+
+    // Set the target encoders position of the wrist slide
+    public void setExtension(int ext) {wristMotor.setTargetPosition(-ext);
+    }
+
+    // Iterate over a list of motors and set them to a provided velocity in ticks/second
+    public void setSlideVelocity(int vel, DcMotorEx... motors) {
+        for (DcMotorEx motor : motors) {
+            motor.setVelocity(vel);
+        }
+    }
+    //TODO : WE need to change all the setGrip Functions this is found in like 3 other classes
+    // Takes a boolean grip value and does the math to convert it to a servo position4
+
+    // true is open false is closed
+    //F Is LEft B is right
+    public void setBothGrip(boolean grip) {
+        //double leftOpen = 0.0, leftClosed = 105.0;
+        // double rightOpen = 270.0, rightClosed = 175.0;
+        double FgripOpen = 13, FgripClosed = 0,
+                BgripOpen = 27, BgripClosed = 0;
+        double servoROT = 300;
+
+        if (grip) {
+            gripServoB.setPosition(BgripOpen/servoROT);
+            gripServoF.setPosition(FgripOpen/servoROT);
+        } else if (!grip) {
+            gripServoB.setPosition(BgripClosed/servoROT);
+            gripServoF.setPosition(FgripClosed/servoROT);
+        }
+    }
+
+    // this is just a setup for the wrist these are random numbers I think we can get the numbers from a demo auto
+    //ill make the auto
+    public  void setFrontGrip (boolean grip) {
+        //double leftOpen = 0.0, leftClosed = 105.0;
+        // double rightOpen = 270.0, rightClosed = 175.0;
+        double FgripOpen = 5, FgripClosed = 15,
+                BgripOpen = 27, BgripClosed = 0;
+        double servoROT = 300;
+
+        if (grip) {
+            gripServoB.setPosition(BgripOpen/servoROT);
+            gripServoF.setPosition(FgripOpen/servoROT);
+        } else if (!grip) {
+            gripServoB.setPosition(BgripClosed/servoROT);
+            gripServoF.setPosition(FgripClosed/servoROT);
+        }
+    }
+
+    public  void setBackGrip (boolean grip) {
+        double BgripOpen = 25, BgripClosed = 0;
+        double servoROT = 300;
+
+        if (grip) {
+
+            gripServoB.setPosition(BgripOpen/servoROT);
+        } else if (!grip) {
+            gripServoB.setPosition(BgripClosed/servoROT);
+        }
+    }
+
 }
